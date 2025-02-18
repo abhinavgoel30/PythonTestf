a3cc5a2e9748e3fdb1839e8ac740443a025c4a24
Neha Garg <nehagarg@Nehas-MacBook-Air.local>
2025-02-18 13:13:14 +0530

    changes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-import process_image  # Import the endpoints module
+
 
 app = FastAPI()
 
-# Include the routes from endpoints.py
-app.include_router(process_image.router)
+
 
 @app.get("/")
 def home():
